d879be0b5b374f3621e7cd0472e1d4a3af906e4e
VISHNUABD <54128003+VISHNUABD@users.noreply.github.com>
2019-08-17 13:16:50 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,i;
+    printf("enter value of n to print n times");
+    scanf("%i",&n);
+    for(i=1;i<=n;i++)
+    {
+        printf(" hello \n");
+    }
+
+    return 0;
+}
